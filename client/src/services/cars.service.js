6decf7294e201f95e7d2d9df35507749c4d469d9
PedroSG00 <suarezgallardopedro@gmail.com>
2022-12-05 19:50:43 +0100

    cars and comments service added, routes fixed

---
+++ client/src/services/cars.service.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+class CarsService {
+
+    constructor() {
+
+        this.api = axios.create({
+            baseURL: `${process.env.REACT_APP_API_URL}/car`
+        })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    getCars() {
+        return this.api.get('/list')
+    }
+
+    getFilteredCars(make, model, year) {
+        return this.api.get(`/?make=${make}&model=${model}&year=${year}`)
+    }
+
+    createCar(carData) {
+        return this.api.post('/create', carData)
+    }
+
+    updateCar(car_id) {
+        this.api.post(`/${car_id}/edit`)
+    }
+
+    deleteCar(car_id) {
+        this.api.post(`/${car_id}/delete`)
+    }
+
+
+}
+
+const carsService = new CarsService()
+
+export default carsService
