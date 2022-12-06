c95242310bd612429ce0a24c11b5c0da14041c8d
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-12-06 17:09:01 +0100

    Homepage Styling

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,16 @@
 import './HomePage.css'
 import SearchTripForm from '../../components/SearchTripForm/SearchTripForm'
 // import Modal from '../../components/Modal/Modal'
+import video from "./../../assets/video.mp4"
+
 
 const HomePage = () => {
 
     return (
         <div className='HomePage'>
-            <SearchTripForm />
-            {/* <Modal /> */}
-        </div>
+            <video src={video} autoPlay muted loop id="video"></video>
+            <SearchTripForm className="search-bar" />
+        </div >
     )
 }
 
