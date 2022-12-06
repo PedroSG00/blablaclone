67310a1170fa6c1eb06fa8b4550f9909dc59749d
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-12-06 15:12:48 +0100

    Fixed searchbar

--- client/src/components/SearchTripForm/SearchTripForm.jsx
+++ client/src/components/SearchTripForm/SearchTripForm.jsx
@@ -1,62 +1,27 @@
 import './SearchTripForm.css'
-import { Button, Row, Col } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
+import { Col, Form, Row, Container, InputGroup, Button } from 'react-bootstrap';
 
 function SearchTripForm() {
 
-
-
-
     return (
-        <div className='SearchTripForm'>
-            <Row>
-                <Col md={{ span: 6, offset: 4 }}>
+        <Container>
+            <Row className='justify-content-center'>
+                <Col md={6}>
                     <Form>
-                        <Row>
-                            <Col md={2}>
-                                <Form.Group className="mb-3" controlId="XXXXXX">
-                                    <Form.Control type="XXXXXX" placeholder="Origin" />
-                                    <Form.Text className="text-muted">
-                                    </Form.Text>
-                                </Form.Group>
-                            </Col>
-                            <Col md={2}>
-                                <Form.Group className="mb-3" controlId="XXXXXX">
-                                    <Form.Control type="XXXXXX" placeholder="Destination" />
-                                    <Form.Text className="text-muted">
-                                    </Form.Text>
-                                </Form.Group>
-                            </Col>
-                            <Col md={2}>
-                                <Form.Group className="mb-2" controlId="XXXXXX">
-                                    <Form.Control type="XXXXXX" placeholder="Date" />
-                                    <Form.Text className="text-muted">
-                                    </Form.Text>
-                                </Form.Group>
-                            </Col>
-                            <Col md={2}>
-
-                                <Form.Group className="mb-3" controlId="XXXXXX">
-                                    <Form.Control type="XXXXXX" placeholder="Passengers" />
-                                    <Form.Text className="text-muted">
-                                    </Form.Text>
-                                </Form.Group>
-                            </Col>
-                            <Col md={2}>
-                                <Button className='bg-color border border-0' type="submit">
-                                    Search
-                                </Button>
-                            </Col>
-
-                        </Row>
+                        <InputGroup>
+                            <Form.Control type='text' placeholder='Origin' ></Form.Control>
+                            <Form.Control type='text' placeholder='Destination'></Form.Control>
+                            <Form.Control type='date' placeholder='Date' required  ></Form.Control>
+                            <Form.Control type='text' ></Form.Control>
+                            <Button>Search</Button>
+                        </InputGroup>
                     </Form>
                 </Col>
-
-
             </Row>
+        </Container>
+    )
 
-        </div >
-    );
+
 
 }
 
