72e76b8965d238a93faca2740a7f1de99083d5d9
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-12-06 12:46:20 +0100

    Added some NavBar styling

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -3,6 +3,8 @@ import { useContext } from 'react'
 import { Nav, Container, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { NavDropdown } from 'react-bootstrap'
+import { MagnifyingGlass, PlusCircle, UserCircle } from "phosphor-react"
+import logo from "./../../assets/logo.png"
 
 const Navigation = () => {
 
@@ -10,15 +12,15 @@ const Navigation = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home" className='text-color'>Rambler</Navbar.Brand>
+                <Navbar.Brand href="#home"><img src={logo} className="app-logo"></img></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className='ms-auto'>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#pricing" className='text-color'>Search</Nav.Link>
-                        <Nav.Link href="#deets">Add trip</Nav.Link>
-                        <NavDropdown title="Profile" id="collasible-nav-dropdown">
+                        <Nav.Link href="#pricing" className='text-color'><MagnifyingGlass size={28} /><span className="nav-title">Search Trips</span></Nav.Link>
+                        <Nav.Link href="#deets" className="text-color"><PlusCircle size={28} /><span className="nav-title">Add Trip</span></Nav.Link>
+                        <NavDropdown title={<div style={{ display: "inline-block" }}><UserCircle size={28} /></div>} id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1" className='text-color'>View profile</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
                                 Another action
