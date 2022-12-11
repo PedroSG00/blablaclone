acfcb84748654219d24e6e826f9ff51618d2b2ed
PedroSG00 <suarezgallardopedro@gmail.com>
2022-12-11 18:06:52 +0100

    CreateCars in process also filter yet

--- client/src/components/CreateCarForm/CreateCarForm.jsx
+++ client/src/components/CreateCarForm/CreateCarForm.jsx
@@ -6,6 +6,13 @@ const CreateCarForm = () => {
 
     const [cars, setCars] = useState([])
     const [make, setMake] = useState([])
+    const [model, setModel] = useState([])
+    const [year, setYear] = useState([])
+
+
+    const [filteredModel, setFilteredModel] = useState([])
+    const [filteredYear, setFilteredYear] = useState([])
+
 
     const getCars = () => {
         carsService
@@ -16,26 +23,48 @@ const CreateCarForm = () => {
 
     const carsMake = cars?.map(elm => (elm.make))
 
+    const carsModel = filteredModel?.map(elm => (elm.model))
+
+    const carsYear = filteredYear?.map(elm => (elm.year))
+
+
     const uniqueCarsMake = [... new Set(carsMake)]
 
+    const uniqueCarsModel = [... new Set(carsModel)]
+
+    const uniqueCarsYear = [... new Set(carsYear)]
+
+
+
     const handleMakeValue = e => setMake(e.value)
+    const handleModelValue = e => setModel(e.value)
+    const handleYearValue = e => setYear(e.value)
+
+
+
 
     const filterCarsMake = () => {
-
-        make
-            &&
-            carsService
-                .getFilteredCars(make)
-                .then((data) => setMake(data))
-                .catch(err => console.log(err))
+        carsService
+            .getFilteredCars(make)
+            .then(({ data }) => setFilteredModel(data))
+            .catch(err => console.log(err))
     }
 
 
+    const filterCarsYear = () => {
+        carsService
+            .getFilteredCars(make, filteredModel)
+            .then(({ data }) => setFilteredYear(data))
+            .catch(err => console.log(err))
+    }
 
     useEffect(() => {
         getCars()
         filterCarsMake()
-    }, [])
+        filterCarsYear()
+    }, [make, model, year])
+
+
 
     return (
         uniqueCarsMake
@@ -45,12 +74,14 @@ const CreateCarForm = () => {
                 options={uniqueCarsMake.map(elm => ({ label: elm, value: elm }))}
                 onChange={handleMakeValue}
             />
-            {/* <Select
-                options={make?.map(elm => ({ label: elm.make, value: elm._id }))}
-            /> */}
-            {/* <Select
-                options={cars.map(elm => ({ label: elm.make, value: elm._id }))}
-            /> */}
+            <Select
+                options={uniqueCarsModel.map(elm => ({ label: elm, value: elm }))}
+                onChange={handleModelValue}
+            />
+            <Select
+                options={uniqueCarsYear.map(elm => ({ label: elm, value: elm }))}
+                onChange={handleYearValue}
+            />
         </>
     )
 }
